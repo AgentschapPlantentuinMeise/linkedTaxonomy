26f55b92f49e1192b04bfad77635b081fd508790
mtrekels <maarten.trekels@plantentuinmeise.be>
2020-06-15 16:46:13 +0200

    start integrating

--- linkedTaxonomy/app.py
+++ linkedTaxonomy/app.py
@@ -9,6 +9,7 @@ import wikidata
 import plazi
 import lt_html
 import html
+import protologue
 
 app = Flask(__name__)
 
@@ -22,10 +23,22 @@ def home(table=None):
 def taxon(genus=None, species=None):
     treatments = plazi.get_treatments(genus,species)
     images = []
+    publications = []
+    type_information = []
+
     for treatment in treatments:
         p,f,t = plazi.get_treatment_information(treatment)
         images.extend(f)
+        publications.extend(p)
+        type_information.extend(t)
+
     image_list = lt_html.include_images(images)
+    for item in type_information:
+        date_p, collections_p = protologue.find_collections(item)
+        print(date_p)
+        print(collections_p)
+    
+    
     return render_template('taxon.html', genus=genus, species=species, image_list=image_list)
 
 
