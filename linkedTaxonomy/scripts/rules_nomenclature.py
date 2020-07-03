3d0854a5f98f3ce4b7197ca9e09dc92446bc0ba3
mtrekels <maarten.trekels@plantentuinmeise.be>
2020-07-03 17:27:54 +0200

    start adding taxonomic rules

--- linkedTaxonomy/scripts/rules_nomenclature.py
+++ linkedTaxonomy/scripts/rules_nomenclature.py
@@ -3,8 +3,31 @@ import plazi
 import type_specimen
 import pandas as pd
 
+def check_unique_holotype(df):
+
+    result = False
+    if len(df.scientificName) == len(df.scientificName.unique():
+            result = True
+
+    return result
+
+def check_date_isotypes(name,date,df):
+
+    result = False
+    df = df[df.scientificName == name]
+    if not df.isempty:
+        for isodate in df.eventDate:
+            if isodate == date:
+                result = True
+
+    return result
+    
+
+
 def execute(genus,species):
 
+    status = 'OK'
+    
     types = types_list = ['Holotype', 'Isotype',
             'Paratype', 'Syntype', 'Lectotype', 'Isolectotype',
             'Neotype', 'Isoneotype', 'Epitype', 'Type']
@@ -18,10 +41,10 @@ def execute(genus,species):
     for item in types:
         types_list[item] = type_specimen.get_types(taxon['taxonKey'],item)
 
-    treatments = plazi.get_treatments(genus,species)
+    # treatments = plazi.get_treatments(genus,species)
+    # lets not focus on the treatments yet
+
+    result = types_list
 
 
-    result = {}
-
-
-    return result
+    return status, result
